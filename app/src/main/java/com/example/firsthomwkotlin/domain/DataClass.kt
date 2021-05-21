883debac6a228c4392cb07da18c19d7623bd941d
aza <azamattemirzhan@gmail.com>
2021-05-22 01:46:16 +0600

    2

--- app/src/main/java/com/example/firsthomwkotlin/domain/DataClass.kt
+++ app/src/main/java/com/example/firsthomwkotlin/domain/DataClass.kt
@@ -1,3 +1,3 @@
 package com.example.firsthomwkotlin.domain
 
-data class DataClass ()
+data class DataClass (val dataTitle: String,val dataInt:Int = 5)
--- app/src/main/java/com/example/firsthomwkotlin/domain/ObjectClass.kt
+++ app/src/main/java/com/example/firsthomwkotlin/domain/ObjectClass.kt
@@ -1,5 +1,9 @@
 package com.example.firsthomwkotlin.domain
 
 object ObjectClass{
+    private val count:Int =5;
 
+    fun getCount():Int{
+        return count
+    }
 }
